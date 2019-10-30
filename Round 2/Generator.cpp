6c9e5b952ac687bc21fbc05043aa936815c1e331
MrChenWithCapsule <Mr.ChenWithCapsule@outlook.com>
2019-10-30 18:38:17 +0800

    Update round 2 data.

--- Round 2/Generator.cpp
+++ Round 2/Generator.cpp
@@ -1,15 +1,15 @@
-#include <random>
 #include <cmath>
 #include <fstream>
-#include <string>
 #include <iomanip>
+#include <random>
+#include <string>
 using namespace std::literals;
 int main()
 {
     std::default_random_engine re{std::random_device{}()};
     int arr[3][10] = {{5, 10, 20, 80, 320, 640, 1000, 1000, 1000, 1000},
                       {1200, 1400, 1600, 1800, 1900, 2000, 2000, 2000, 2000, 2000},
-                      {2200, 2200, 2400, 2600, 2800, 3000, 3000, 3000, 3000, 3000}};
+                      {3000, 5000, 8000, 11000, 14000, 15000, 15000, 15000, 15000, 15000}};
     for (int i = 0; i < 3; ++i)
     {
         char sym;
@@ -35,7 +35,8 @@ int main()
             int current = 0, max_n = 0;
             while (current < arr[i][j])
             {
-                int n = std::uniform_int_distribution<>{1, std::min(arr[i][j] - current, std::max(arr[i][j] / 5, 100))}(re);
+                int n =
+                    std::uniform_int_distribution<>{1, std::min(arr[i][j] - current, std::max(arr[i][j] / 5, 100))}(re);
                 current += n;
                 max_n = std::max(max_n, n);
                 double a = -genline(re), b = genline(re);
@@ -46,7 +47,8 @@ int main()
                 while (n--)
                 {
                     tmp += delta;
-                    fout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10) << tmp << ' ' << a * tmp * tmp + b * tmp << '\n';
+                    fout << std::fixed << std::setprecision(std::numeric_limits<double>::digits10) << tmp << ' '
+                         << a * tmp * tmp + b * tmp << '\n';
                 }
             }
             ansout << max_n << '\n';
